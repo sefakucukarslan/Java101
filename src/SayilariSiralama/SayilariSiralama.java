bb083a39cce9afb8e7a2f903fab44a25d34bf108
Sefa <100466472+Sefa96@users.noreply.github.com>
2022-08-15 20:00:13 +0300

    SayilariSiralama

---
+++ src/SayilariSiralama/SayilariSiralama.java
@@ -0,0 +1,44 @@
+package SayilariSiralama;
+
+import java.util.Scanner;
+
+public class SayilariSiralama {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner scr = new Scanner(System.in);
+
+        System.out.println("1. Sayı : ");
+        a = scr.nextInt();
+
+        System.out.println("2. Sayı : ");
+        b = scr.nextInt();
+
+        System.out.println("3. Sayı : ");
+        c = scr.nextInt();
+
+        if ((a > b) && (a > c)){
+            if (b > c){
+                System.out.println("a > b > c");
+            }
+            else{
+                System.out.println("a > c > b");
+            }
+        } else if ((b > a) && (b > c)) {
+            if (a > c){
+                System.out.println("b > a > c");
+            }
+            else {
+                System.out.println("b > c > a");
+            }
+        }
+        else {
+            if (a > b){
+                System.out.println("c > a > b");
+            }
+            else {
+                System.out.println("c > b > a");
+            }
+        }
+
+    }
+}
