aed9f0ae5317461f5f0183b3566c29912d9e94db
Sefa <100466472+Sefa96@users.noreply.github.com>
2022-08-17 20:43:31 +0300

    Tek Sayıların Toplamını Bulan Program

---
+++ src/TekSayilarinToplami/TekToplam.java
@@ -0,0 +1,18 @@
+package TekSayilarinToplami;
+
+import java.util.Scanner;
+
+public class TekToplam {
+    public static void main(String[] args) {
+        int number, total =0;
+        do {
+            Scanner inp = new Scanner(System.in);
+            System.out.print("Sayı Gir: ");
+            number = inp.nextInt();
+            if (number % 2 == 0 && number % 4 == 0) {
+                total += number;
+            }
+        }while (number % 2 == 0) ;
+        System.out.println("Toplam= " + total);
+    }
+}
