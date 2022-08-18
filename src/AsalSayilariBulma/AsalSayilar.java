3f0f91539e1ee8cdd2e93eef6d4a5cd603bfa5c6
Sefa <100466472+Sefa96@users.noreply.github.com>
2022-08-18 18:29:46 +0300

    Ters Üçgen Yapımı

---
+++ src/AsalSayilariBulma/AsalSayilar.java
@@ -0,0 +1,17 @@
+package AsalSayilariBulma;
+
+public class AsalSayilar {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            boolean asal = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    asal =  false;
+                }
+            }
+            if (asal) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
