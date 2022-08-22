2e7769cbb440665f07cf5c2bb93028a44973a054
Sefa <100466472+Sefa96@users.noreply.github.com>
2022-08-22 09:36:36 +0300

    Mayın Tarlası Oyunu

---
+++ src/MayinTarlasi/MayinTarlasi.java
@@ -0,0 +1,9 @@
+package MayinTarlasi;
+
+public class MayinTarlasi {
+    public static void main(String[] args) {
+
+        MineSweeper sweeper = new MineSweeper();
+        sweeper.run();
+    }
+}
